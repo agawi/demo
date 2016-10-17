f43bf6e31e57aa9e37f49cd900aa89adf03aba43
Arnaud Gawinowski <a.gawi2109@gmail.com>
2016-10-16 19:13:09 -0700

    Add styling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,38 @@
 class CommentsController < ApplicationController
+  before_action :set_post
   def create
-    @post = Post.find(:post_id)
     @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
 
   def destroy
-    @post = Post.find(params[:post])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to post_path(@post)
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:body, :commenter)
+    end
+
+    def set_post
+      @post = Post.find(params[:post_id])
     end
 end
