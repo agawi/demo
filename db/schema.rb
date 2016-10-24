5737c342c212d9a03675d7a2c833535e691e950e
Arnaud Gawinowski <a.gawi2109@gmail.com>
2016-10-23 17:13:03 -0700

    fix bug on development

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023024228) do
+ActiveRecord::Schema.define(version: 20161024001103) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -51,6 +51,10 @@ ActiveRecord::Schema.define(version: 20161023024228) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.string   "user_name"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
